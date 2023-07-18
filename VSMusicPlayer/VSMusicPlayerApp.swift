42e7b2364157650b0b79449161183f2fda819e31
bilal simsek <xyzbilal@gmail.com>
2023-07-18 10:40:50 +0300

    Initial Commit

---
+++ VSMusicPlayer/VSMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VSMusicPlayerApp.swift
+//  VSMusicPlayer
+//
+//  Created by Bilal SIMSEK on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct VSMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
